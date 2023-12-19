970d9e29d3c761d4930903ba48bf313bab9744d1
Manju <nkmanju412@gmail.com>
2023-12-19 15:50:03 +0530

    Deployment file modified

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,8 +59,8 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-06dd92ecc74fdfb36"
-  instance_type          = "t2.medium"
+  ami                    = "ami-0ec7f9846da6b0f61"
+  instance_type          = "t2.2xlarge"
   key_name               = "key_file"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})

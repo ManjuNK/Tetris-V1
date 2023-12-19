9491203c71685724a985f1d7a8d30a7adf68ca36
Manju <nkmanju412@gmail.com>
2023-12-19 14:40:06 +0530

    backend file modified

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tetris_bkt" # Replace with your actual S3 bucket name
+    bucket = "tetris-bkt" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "eu-central-1"
   }
